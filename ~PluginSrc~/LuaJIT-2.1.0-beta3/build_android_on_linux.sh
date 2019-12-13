4e93df29d6db180ed5548437a99ac5d4fc947b40
Silas <hdss3609@gmail.com>
2019-12-13 12:51:18 +0800

    将Linux安卓的编译脚本拆分一个WSL的出来

--- ~PluginSrc~/LuaJIT-2.1.0-beta3/build_android_on_linux.sh
+++ ~PluginSrc~/LuaJIT-2.1.0-beta3/build_android_on_linux.sh
@@ -4,23 +4,23 @@
 LUAJIT=.
 BUILD_DIR=$LUAJIT/build
 
-# #编译 android-x86
-# make clean
-# NDK=`pwd`/ndk
-# NDKAPI=14
-# NDKTRIPLE=x86
-# NDKVER=$NDK/toolchains/$NDKTRIPLE-4.9
-# NDKP=$NDKVER/prebuilt/linux-x86_64/bin/i686-linux-android-
-# NDKF="-isystem $NDK/sysroot/usr/include/i686-linux-android -D__ANDROID_API__=$NDKAPI"
-# NDK_SYSROOT_BUILD=$NDK/sysroot
-# NDK_SYSROOT_LINK=$NDK/platforms/android-$NDKAPI/arch-x86
+#编译 android-x86
+make clean
+NDK=`pwd`/ndk
+NDKAPI=14
+NDKTRIPLE=x86
+NDKVER=$NDK/toolchains/$NDKTRIPLE-4.9
+NDKP=$NDKVER/prebuilt/linux-x86_64/bin/i686-linux-android-
+NDKF="-isystem $NDK/sysroot/usr/include/i686-linux-android -D__ANDROID_API__=$NDKAPI"
+NDK_SYSROOT_BUILD=$NDK/sysroot
+NDK_SYSROOT_LINK=$NDK/platforms/android-$NDKAPI/arch-x86
 
-# make HOST_CC="gcc -m32 " CROSS=$NDKP TARGET_FLAGS="$NDKF" TARGET_SYS=Linux TARGET_SHLDFLAGS="--sysroot $NDK_SYSROOT_LINK"  TARGET_LDFLAGS="--sysroot $NDK_SYSROOT_LINK" TARGET_CFLAGS="--sysroot $NDK_SYSROOT_BUILD" TARGET_LFSFLAGS=""
+make HOST_CC="gcc -m32 " CROSS=$NDKP TARGET_FLAGS="$NDKF" TARGET_SYS=Linux TARGET_SHLDFLAGS="--sysroot $NDK_SYSROOT_LINK"  TARGET_LDFLAGS="--sysroot $NDK_SYSROOT_LINK" TARGET_CFLAGS="--sysroot $NDK_SYSROOT_BUILD" TARGET_LFSFLAGS=""
 
-# OUTPUT=$BUILD_DIR/android/x86
-# rm -rf $OUTPUT
-# mkdir -p $OUTPUT
-# mv ./src/libluajit.so $OUTPUT/libluajit.so
+OUTPUT=$BUILD_DIR/android/x86
+rm -rf $OUTPUT
+mkdir -p $OUTPUT
+mv ./src/libluajit.so $OUTPUT/libluajit.so
 
 # #编译 android-armeabi
 # make clean
@@ -41,24 +41,24 @@ BUILD_DIR=$LUAJIT/build
 # mkdir -p $OUTPUT
 # mv ./src/libluajit.so $OUTPUT/libluajit.so
 
-# #编译 android-armeabi-v7a
-# make clean
-# NDK=`pwd`/ndk
-# NDKAPI=14
-# NDKTRIPLE=arm-linux-androideabi
-# NDKVER=$NDK/toolchains/$NDKTRIPLE-4.9
-# NDKP=$NDKVER/prebuilt/linux-x86_64/bin/$NDKTRIPLE-
-# NDKF="-isystem $NDK/sysroot/usr/include/$NDKTRIPLE -D__ANDROID_API__=$NDKAPI"
-# NDK_SYSROOT_BUILD=$NDK/sysroot
-# NDK_SYSROOT_LINK=$NDK/platforms/android-$NDKAPI/arch-arm
-# NDKARCH="-march=armv7-a -mfloat-abi=softfp -Wl,--fix-cortex-a8"
+#编译 android-armeabi-v7a
+make clean
+NDK=`pwd`/ndk
+NDKAPI=14
+NDKTRIPLE=arm-linux-androideabi
+NDKVER=$NDK/toolchains/$NDKTRIPLE-4.9
+NDKP=$NDKVER/prebuilt/linux-x86_64/bin/$NDKTRIPLE-
+NDKF="-isystem $NDK/sysroot/usr/include/$NDKTRIPLE -D__ANDROID_API__=$NDKAPI"
+NDK_SYSROOT_BUILD=$NDK/sysroot
+NDK_SYSROOT_LINK=$NDK/platforms/android-$NDKAPI/arch-arm
+NDKARCH="-march=armv7-a -mfloat-abi=softfp -Wl,--fix-cortex-a8"
 
-# make HOST_CC="gcc -m32 " CROSS=$NDKP TARGET_FLAGS="$NDKF $NDKARCH" TARGET_SYS=Linux TARGET_SHLDFLAGS="--sysroot $NDK_SYSROOT_LINK"  TARGET_LDFLAGS="--sysroot $NDK_SYSROOT_LINK" TARGET_CFLAGS="--sysroot $NDK_SYSROOT_BUILD" TARGET_LFSFLAGS=""
+make HOST_CC="gcc -m32 " CROSS=$NDKP TARGET_FLAGS="$NDKF $NDKARCH" TARGET_SYS=Linux TARGET_SHLDFLAGS="--sysroot $NDK_SYSROOT_LINK"  TARGET_LDFLAGS="--sysroot $NDK_SYSROOT_LINK" TARGET_CFLAGS="--sysroot $NDK_SYSROOT_BUILD" TARGET_LFSFLAGS=""
 
-# OUTPUT=$BUILD_DIR/android/armeabi-v7a
-# rm -rf $OUTPUT
-# mkdir -p $OUTPUT
-# mv ./src/libluajit.so $OUTPUT/libluajit.so
+OUTPUT=$BUILD_DIR/android/armeabi-v7a
+rm -rf $OUTPUT
+mkdir -p $OUTPUT
+mv ./src/libluajit.so $OUTPUT/libluajit.so
 
 #编译 android-arm64-v8a
 make clean
