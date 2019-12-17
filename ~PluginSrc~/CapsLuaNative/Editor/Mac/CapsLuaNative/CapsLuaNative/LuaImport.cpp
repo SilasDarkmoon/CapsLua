ffbf45c0fe9192f0c668c12d5718b843de4429ef
Silas <Silas@Silass-Mac-mini.local>
2019-12-17 20:35:18 +0800

    重新编译了几个Mac Editor下的so。现在能在Mac Editor 下正常跑了

--- ~PluginSrc~/CapsLuaNative/Editor/Mac/CapsLuaNative/CapsLuaNative/LuaImport.cpp
+++ ~PluginSrc~/CapsLuaNative/Editor/Mac/CapsLuaNative/CapsLuaNative/LuaImport.cpp
@@ -9,14 +9,15 @@ extern "C" {
 
 void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API UnityPluginLoad(IUnityInterfaces *unityInterfaces)
 {
-#if __cplusplus
-    UnityInterfaceGUID guidlua(0xE472D7060BA74533UL, 0x88F82C3A4ADD9BBFUL);
-#else
-    UnityInterfaceGUID guidlua;
-    guidlua.m_GUIDHigh = 0xE472D7060BA74533UL;
-    guidlua.m_GUIDLow = 0x88F82C3A4ADD9BBFUL;
-#endif
-    g_pLuaPluginInterface = (LuaPluginInterface*)unityInterfaces->GetInterface(guidlua);
+//#if __cplusplus
+//    UnityInterfaceGUID guidlua(0xE472D7060BA74533UL, 0x88F82C3A4ADD9BBFUL);
+//#else
+//    UnityInterfaceGUID guidlua;
+//    guidlua.m_GUIDHigh = 0xE472D7060BA74533UL;
+//    guidlua.m_GUIDLow = 0x88F82C3A4ADD9BBFUL;
+//#endif
+//    g_pLuaPluginInterface = (LuaPluginInterface*)unityInterfaces->GetInterface(guidlua);
+    g_pLuaPluginInterface = (LuaPluginInterface*)unityInterfaces->GetInterfaceSplit(0xE472D7060BA74533UL, 0x88F82C3A4ADD9BBFUL);
 }
 
 int UNITY_INTERFACE_EXPORT IsReady()
