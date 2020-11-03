28691b74f54877dac613d284697691d74b541093
Silas <hdss3609@gmail.com>
2020-11-03 20:46:59 +0800

    clr.randomstate

--- Runtime/LuaExt/CapsLuaExtFramework.cs
+++ Runtime/LuaExt/CapsLuaExtFramework.cs
@@ -55,6 +55,8 @@ namespace Capstones.LuaExt
                         L.SetField(-2, "splitstr");
                         L.pushcfunction(ClrDelCurrentLua);
                         L.SetField(-2, "thislua");
+                        L.pushcfunction(ClrDelRandomState);
+                        L.SetField(-2, "randomstate");
                         L.PushString(ThreadSafeValues.UpdatePath);
                         L.SetField(-2, "updatepath");
                         L.pushcfunction(ClrDelGetLangValueOfUserDataType);
@@ -188,6 +190,7 @@ namespace Capstones.LuaExt
         public static readonly lua.CFunction ClrDelGetCapID = new lua.CFunction(ClrFuncGetCapID);
         public static readonly lua.CFunction ClrDelSplitStr = new lua.CFunction(ClrFuncSplitStr);
         public static readonly lua.CFunction ClrDelCurrentLua = new lua.CFunction(ClrFuncCurrentLua);
+        public static readonly lua.CFunction ClrDelRandomState = new lua.CFunction(ClrFuncRandomState);
         public static readonly lua.CFunction ClrDelGetLangValueOfUserDataType = new lua.CFunction(ClrFuncGetLangValueOfUserDataType);
         public static readonly lua.CFunction ClrDelGetLangValueOfStringType = new lua.CFunction(ClrFuncGetLangValueOfStringType);
         public static readonly lua.CFunction ClrDelUpdateLanguageConverter = new lua.CFunction(UpdateLanguageConverter);
@@ -417,6 +420,345 @@ namespace Capstones.LuaExt
         {
             l.pushlightuserdata(l);
             return 1;
+        }
+        [AOT.MonoPInvokeCallback(typeof(lua.CFunction))]
+        public static int ClrFuncRandomState(IntPtr l)
+        {
+            int type;
+            IntPtr ps = IntPtr.Zero;
+            using (var lr = l.CreateStackRecover())
+            {
+                l.pushcfunction(LuaHub.LuaFuncOnError); // err
+                l.GetGlobal("debug"); // TODO: import lua_getupvalue
+                l.GetField(-1, "getupvalue");
+                l.remove(-2); // err getupvalue
+                l.GetGlobal("math");
+                l.GetField(-1, "random");
+                l.GetField(-2, "randomcaps");
+                if (l.isfunction(-1))
+                {
+                    type = 2;
+                }
+                else
+                {
+                    type = 1;
+                }
+                l.pop(1);
+                l.remove(-2); // err getupvalue random
+                l.pushnumber(1); // err getupvalue ramdom 1
+                l.pcall(2, 2, -4);
+                if (!l.isuserdata(-1))
+                {
+                    type = 0;
+                    return 0;
+                }
+                else
+                {
+                    ps = l.touserdata(-1);
+                }
+            }
+
+            if (l.gettop() == 0)
+            { // get random state
+                l.newtable();
+                if (type == 1)
+                {
+                    var v0l = (uint)Marshal.ReadInt32(ps);
+                    l.pushnumber(v0l);
+                    l.SetField(-2, "v0l");
+                    var v0h = (uint)Marshal.ReadInt32(ps, 4);
+                    l.pushnumber(v0h);
+                    l.SetField(-2, "v0h");
+                    var v1l = (uint)Marshal.ReadInt32(ps, 8);
+                    l.pushnumber(v1l);
+                    l.SetField(-2, "v1l");
+                    var v1h = (uint)Marshal.ReadInt32(ps, 12);
+                    l.pushnumber(v1h);
+                    l.SetField(-2, "v1h");
+                    var v2l = (uint)Marshal.ReadInt32(ps, 16);
+                    l.pushnumber(v2l);
+                    l.SetField(-2, "v2l");
+                    var v2h = (uint)Marshal.ReadInt32(ps, 20);
+                    l.pushnumber(v2h);
+                    l.SetField(-2, "v2h");
+                    var v3l = (uint)Marshal.ReadInt32(ps, 24);
+                    l.pushnumber(v3l);
+                    l.SetField(-2, "v3l");
+                    var v3h = (uint)Marshal.ReadInt32(ps, 28);
+                    l.pushnumber(v3h);
+                    l.SetField(-2, "v3h");
+                    return 1;
+                }
+                else
+                {
+                    var x0 = (ushort)(uint)Marshal.ReadInt32(ps);
+                    l.pushnumber(x0);
+                    l.SetField(-2, "x0");
+                    var x1 = (ushort)(uint)Marshal.ReadInt32(ps, 4);
+                    l.pushnumber(x1);
+                    l.SetField(-2, "x1");
+                    var x2 = (ushort)(uint)Marshal.ReadInt32(ps, 8);
+                    l.pushnumber(x2);
+                    l.SetField(-2, "x2");
+                    var a0 = (uint)Marshal.ReadInt32(ps, 12);
+                    l.pushnumber(a0);
+                    l.SetField(-2, "a0");
+                    var a1 = (uint)Marshal.ReadInt32(ps, 16);
+                    l.pushnumber(a1);
+                    l.SetField(-2, "a1");
+                    var a2 = (uint)Marshal.ReadInt32(ps, 20);
+                    l.pushnumber(a2);
+                    l.SetField(-2, "a2");
+                    var c = (uint)Marshal.ReadInt32(ps, 24);
+                    l.pushnumber(c);
+                    l.SetField(-2, "c");
+
+                    long unite = x2;
+                    unite <<= 16;
+                    unite += x1;
+                    unite <<= 16;
+                    unite += x0;
+
+                    l.pushnumber(unite);
+                    return 2;
+                }
+            }
+            else if (l.istable(1))
+            { // set random state
+                if (type == 1)
+                {
+                    l.GetField(1, "v0l");
+                    var v0l = (uint)l.tonumber(-1);
+                    Marshal.WriteInt32(ps, (int)v0l);
+                    l.pop(1);
+                    l.GetField(1, "v0h");
+                    var v0h = (uint)l.tonumber(-1);
+                    Marshal.WriteInt32(ps, 4, (int)v0h);
+                    l.pop(1);
+                    l.GetField(1, "v1l");
+                    var v1l = (uint)l.tonumber(-1);
+                    Marshal.WriteInt32(ps, 8, (int)v1l);
+                    l.pop(1);
+                    l.GetField(1, "v1h");
+                    var v1h = (uint)l.tonumber(-1);
+                    Marshal.WriteInt32(ps, 12, (int)v1h);
+                    l.pop(1);
+                    l.GetField(1, "v2l");
+                    var v2l = (uint)l.tonumber(-1);
+                    Marshal.WriteInt32(ps, 16, (int)v2l);
+                    l.pop(1);
+                    l.GetField(1, "v2h");
+                    var v2h = (uint)l.tonumber(-1);
+                    Marshal.WriteInt32(ps, 20, (int)v2h);
+                    l.pop(1);
+                    l.GetField(1, "v3l");
+                    var v3l = (uint)l.tonumber(-1);
+                    Marshal.WriteInt32(ps, 24, (int)v3l);
+                    l.pop(1);
+                    l.GetField(1, "v3h");
+                    var v3h = (uint)l.tonumber(-1);
+                    Marshal.WriteInt32(ps, 28, (int)v3h);
+                    l.pop(1);
+                }
+                else
+                {
+                    l.GetField(1, "x0");
+                    var x0 = (uint)l.tonumber(-1);
+                    Marshal.WriteInt32(ps, (int)x0);
+                    l.pop(1);
+                    l.GetField(1, "x1");
+                    var x1 = (uint)l.tonumber(-1);
+                    Marshal.WriteInt32(ps, 4, (int)x1);
+                    l.pop(1);
+                    l.GetField(1, "x2");
+                    var x2 = (uint)l.tonumber(-1);
+                    Marshal.WriteInt32(ps, 8, (int)x2);
+                    l.pop(1);
+                    l.GetField(1, "a0");
+                    var a0 = (uint)l.tonumber(-1);
+                    Marshal.WriteInt32(ps, 12, (int)a0);
+                    l.pop(1);
+                    l.GetField(1, "a1");
+                    var a1 = (uint)l.tonumber(-1);
+                    Marshal.WriteInt32(ps, 16, (int)a1);
+                    l.pop(1);
+                    l.GetField(1, "a2");
+                    var a2 = (uint)l.tonumber(-1);
+                    Marshal.WriteInt32(ps, 20, (int)a2);
+                    l.pop(1);
+                    l.GetField(1, "c");
+                    var c = (uint)l.tonumber(-1);
+                    Marshal.WriteInt32(ps, 24, (int)c);
+                    l.pop(1);
+                }
+                return 0;
+            }
+            else if (l.isnumber(1) && type == 2)
+            {
+                long unite = (long)l.tonumber(1);
+                uint x0 = (uint)(unite & 0xFFFFL);
+                Marshal.WriteInt32(ps, (int)x0);
+                uint x1 = (uint)((unite & (0xFFFFL << 16)) >> 16);
+                Marshal.WriteInt32(ps, 4, (int)x1);
+                uint x2 = (uint)((unite & (0xFFFFL << 32)) >> 32);
+                Marshal.WriteInt32(ps, 4, (int)x2);
+                return 0;
+            }
+            else
+            { // don't know what to do.
+                return 0;
+            }
+        }
+
+        [StructLayout(LayoutKind.Explicit)]
+        public struct LuaRandomState
+        {
+            [StructLayout(LayoutKind.Sequential)]
+            public struct LuaRandomStateEmpty
+            {
+            }
+            [StructLayout(LayoutKind.Sequential)]
+            public struct LuaRandomStateLuaJit
+            {
+                public ulong v0;
+                public ulong v1;
+                public ulong v2;
+                public ulong v3;
+            }
+            [StructLayout(LayoutKind.Sequential)]
+            public struct LuaRandomStateLuaJitSplit
+            {
+                public uint v0l;
+                public uint v0h;
+                public uint v1l;
+                public uint v1h;
+                public uint v2l;
+                public uint v2h;
+                public uint v3l;
+                public uint v3h;
+            }
+            [StructLayout(LayoutKind.Sequential)]
+            public struct LuaRandomStateCaps
+            {
+                public ushort x0;
+                public ushort x1;
+                public ushort x2;
+                private ushort xreserved;
+                public uint a0;
+                public uint a1;
+                public uint a2;
+                public uint c;
+            }
+
+            [FieldOffset(0)]
+            public LuaRandomStateEmpty Empty;
+            [FieldOffset(0)]
+            public LuaRandomStateLuaJit LuaJit;
+            [FieldOffset(0)]
+            public LuaRandomStateLuaJitSplit LuaJitSplit;
+            [FieldOffset(0)]
+            public LuaRandomStateCaps Caps;
+            [FieldOffset(0)]
+            public long CapsUnite;
+
+            [FieldOffset(32)]
+            public byte Type; // 0 - Empty; 1 - LuaJit; 2 - Caps;
+        }
+        public static LuaRandomState GetRandomState(IntPtr l)
+        {
+            LuaRandomState state = new LuaRandomState();
+            using (var lr = l.CreateStackRecover())
+            {
+                l.pushcfunction(LuaHub.LuaFuncOnError); // err
+                l.GetGlobal("debug"); // TODO: import lua_getupvalue
+                l.GetField(-1, "getupvalue");
+                l.remove(-2); // err getupvalue
+                l.GetGlobal("math");
+                l.GetField(-1, "random");
+                l.GetField(-2, "randomcaps");
+                if (l.isfunction(-1))
+                {
+                    state.Type = 2;
+                }
+                else
+                {
+                    state.Type = 1;
+                }
+                l.pop(1);
+                l.remove(-2); // err getupvalue random
+                l.pushnumber(1); // err getupvalue random 1
+                l.pcall(2, 2, -4);
+                if (!l.isuserdata(-1))
+                {
+                    state.Type = 0;
+                }
+                else
+                {
+                    IntPtr ps = l.touserdata(-1);
+                    if (state.Type == 1)
+                    {
+                        state.LuaJit.v0 = (ulong)Marshal.ReadInt64(ps);
+                        state.LuaJit.v1 = (ulong)Marshal.ReadInt64(ps, 8);
+                        state.LuaJit.v2 = (ulong)Marshal.ReadInt64(ps, 16);
+                        state.LuaJit.v3 = (ulong)Marshal.ReadInt64(ps, 24);
+                    }
+                    else
+                    {
+                        state.Caps.x0 = (ushort)(uint)Marshal.ReadInt32(ps);
+                        state.Caps.x1 = (ushort)(uint)Marshal.ReadInt32(ps, 4);
+                        state.Caps.x2 = (ushort)(uint)Marshal.ReadInt32(ps, 8);
+                        state.Caps.a0 = (uint)Marshal.ReadInt32(ps, 12);
+                        state.Caps.a1 = (uint)Marshal.ReadInt32(ps, 16);
+                        state.Caps.a2 = (uint)Marshal.ReadInt32(ps, 20);
+                        state.Caps.c = (uint)Marshal.ReadInt32(ps, 24);
+                    }
+                }
+            }
+            return state;
+        }
+        public static void SetRandomState(IntPtr l, LuaRandomState state)
+        {
+            if (state.Type == 0)
+            {
+                return;
+            }
+            using (var lr = l.CreateStackRecover())
+            {
+                l.pushcfunction(LuaHub.LuaFuncOnError); // err
+                l.GetGlobal("debug"); // TODO: import lua_getupvalue
+                l.GetField(-1, "getupvalue");
+                l.remove(-2); // err getupvalue
+                l.GetGlobal("math");
+                l.GetField(-1, "random");
+                l.remove(-2); // err getupvalue random
+                l.pushnumber(1); // err getupvalue random 1
+                l.pcall(2, 2, -4);
+                if (!l.isuserdata(-1))
+                {
+                    return;
+                }
+                else
+                {
+                    IntPtr ps = l.touserdata(-1);
+                    if (state.Type == 1)
+                    {
+                        Marshal.WriteInt64(ps, (long)state.LuaJit.v0);
+                        Marshal.WriteInt64(ps, 8, (long)state.LuaJit.v1);
+                        Marshal.WriteInt64(ps, 16, (long)state.LuaJit.v2);
+                        Marshal.WriteInt64(ps, 24, (long)state.LuaJit.v3);
+                    }
+                    else if (state.Type == 2)
+                    {
+                        Marshal.WriteInt32(ps, (int)(uint)state.Caps.x0);
+                        Marshal.WriteInt32(ps, 4, (int)(uint)state.Caps.x1);
+                        Marshal.WriteInt32(ps, 8, (int)(uint)state.Caps.x2);
+                        Marshal.WriteInt32(ps, 12, (int)state.Caps.a0);
+                        Marshal.WriteInt32(ps, 16, (int)state.Caps.a1);
+                        Marshal.WriteInt32(ps, 20, (int)state.Caps.a2);
+                        Marshal.WriteInt32(ps, 24, (int)state.Caps.c);
+                    }
+                }
+            }
         }
 
         private static int ClrFuncGetLangValue(IntPtr l, bool isStringType)
